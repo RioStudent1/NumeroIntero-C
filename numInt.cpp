081395386caa4b75fdf029a3a7d7dad616f41562
RioStudent <manuelcacciamani1@gmail.com>
2026-07-28 13:31:04 +0200

    Add files via upload

---
+++ numInt.cpp
@@ -0,0 +1,21 @@
+// Includo le direttive
+
+#include <iostream>
+using namespace std;
+
+// Funzione principale Main
+
+int main() {
+
+    int num;
+
+    // Inserimento numero e assegnazione alla variabile num
+
+    cout << "Inserisci un numero intero, poi premi invio: ";
+    cin >> num;
+
+    // Mostra il valore del numero inserito
+    
+    cout << "Numero inserito: " << num;
+
+}
